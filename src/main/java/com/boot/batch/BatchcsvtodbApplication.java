2a72a43390351c563362ad94cd86b34078327d87
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-11-01 14:16:59 +0530

    Initial Commit

---
+++ src/main/java/com/boot/batch/BatchcsvtodbApplication.java
@@ -0,0 +1,13 @@
+package com.boot.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchcsvtodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchcsvtodbApplication.class, args);
+	}
+
+}
