ddfe311cc39eefad5c2c6b7f8655eb55eb79d18b
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-11-01 16:31:12 +0530

    Updated Config and Test

--- src/main/java/com/boot/batch/config/BatchConfig.java
+++ src/main/java/com/boot/batch/config/BatchConfig.java
@@ -1,5 +1,12 @@
 package com.boot.batch.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -9,17 +16,42 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.boot.batch.model.Product;
 
 @Configuration
 public class BatchConfig {
 	
+	@Autowired
+	private JobBuilderFactory jbf;
+	
+	@Autowired
+	private StepBuilderFactory sbf;
+	
+	public Job job() {
+		return jbf.get("j2")
+		.incrementer(new RunIdIncrementer())
+		.start(step())
+		.build();
+	}
+	
+	@Bean
+	public Step step() {
+		return sbf.get("s1")
+				.<Product,Product>chunk(3)
+				.reader(reader())
+				.processor(processor())
+				.writer(writer())
+				.build();
+	}
 	@Bean
 	public ItemReader<Product> reader(){
+		
 		//Create reader instance
 		FlatFileItemReader<Product> reader = new FlatFileItemReader<>();
 		//Set input file location
@@ -47,16 +79,25 @@ public class BatchConfig {
 		
 	}
 	
-	
+	@Bean
 	public ItemWriter<Product> writer(){
-		JdbcBatchItemWriter<Product> Itemwriter = new JdbcBatchItemWriter<>();
-		Itemwriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Product>());
-		Itemwriter.setSql("INSERT INTO PRODUCT (ID, NAME,DESCRIPTION,PRICE) VALUES (:id,:name,:description,:price )");
+		JdbcBatchItemWriter<Product> writer = new JdbcBatchItemWriter<>();
+		writer.setDataSource(dataSource());
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Product>());
+		writer.setSql("INSERT INTO PRODUCT (ID, NAME,DESCRIPTION,PRICE) VALUES (:id,:name,:description,:price )");
 		
-		return Itemwriter;
+		return writer;
 	}
 	
-	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+		dataSource.setUsername("root");
+		dataSource.setPassword("Venky1234@");
+		return dataSource();
+	}
 	
 }
 
